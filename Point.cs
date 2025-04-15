d0b9e5e27f4f7bb9f89536e73b200c94d6031e08
carter-oleary <carter.m.oleary@outlook.com>
2025-04-15 14:30:35 -0400

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,33 @@
+﻿class Point
+{
+    public int X {  get; set; }
+    public int Y { get; set; }
+
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public string GetQuadrant()
+    {
+        switch(X, Y)
+        {
+            case (< 0, < 0):
+                return "Third";
+            case ( < 0, > 0):
+                return "Fourth";
+            case (>0, < 0):
+                return "Second";
+            default:
+                return "First";
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Point p1 = new Point(1, 1);
+Point p2 = new Point(-1, 1);
+Point p3 = new Point(-1, -1);
+Point p4 = new Point(1, -1);
+Point[] pointArr = {p1, p2, p3, p4};
+
+for (int i = 0; i < pointArr.Length; i++)
+{
+    Console.WriteLine($"Point {i+1} is in the {pointArr[i].GetQuadrant()} quadrant");
+}
